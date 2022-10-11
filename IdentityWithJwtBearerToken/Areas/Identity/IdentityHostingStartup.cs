ef7d9f9677a866084c8eeb427671059d78d7537e
Emerson Melo <emerson@t2s.com.br>
2022-10-11 15:06:15 -0300

    Usando o identity com o banco de dados SQLite

--- IdentityWithJwtBearerToken/Areas/Identity/IdentityHostingStartup.cs
+++ IdentityWithJwtBearerToken/Areas/Identity/IdentityHostingStartup.cs
@@ -17,14 +17,20 @@ namespace IdentityWithJwtBearerToken.Areas.Identity
         {
             builder.ConfigureServices((context, services) => {
                 services.AddDbContext<IdentityWithJwtBearerTokenContext>(options =>
-                    options.UseSqlServer(
-                        context.Configuration.GetConnectionString("IdentityWithJwtBearerTokenContextConnection")));
+                //options.UseSqlServer(
+                //    context.Configuration.GetConnectionString("IdentityWithJwtBearerTokenContextConnection")));
+
+
+                // exemplo de uso om sqlite
+                // https://jcooney.net/post/2016/11/13/SqliteIdentity.html
+                options.UseSqlite(
+                        context.Configuration.GetConnectionString("SqliteExemploDiretorioConnection")));
 
                 // 43:55 ao gerar o startup, ele gera automaticamente com padrão  options.SignIn.RequireConfirmedAccount = true
                 // isso faz com que somente contas confirmadas na tabela de dados consigam fazer login
                 //
                 // alteramos essa configuração para FALSE conforme video
-                
+
                 services.AddDefaultIdentity<IdentityWithJwtBearerTokenUser>(options => options.SignIn.RequireConfirmedAccount = false)
                     .AddEntityFrameworkStores<IdentityWithJwtBearerTokenContext>();
             });
